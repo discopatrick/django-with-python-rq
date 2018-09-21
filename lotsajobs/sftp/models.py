8e503c7d13749e25b857e1f3b5c13b787be2c6d2
Patrick Heneghan <discopatrick@gmail.com>
2018-09-21 10:35:59 +0100

    Add `Response` model

--- lotsajobs/sftp/models.py
+++ lotsajobs/sftp/models.py
@@ -2,8 +2,16 @@ import uuid
 
 from django.db import models
 
+
 class Request(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4)
 
     def __str__(self):
         return f'Request object ({self.uuid})'
+
+
+class Response(models.Model):
+    uuid = models.UUIDField()
+
+    def __str__(self):
+        return f'Response object ({self.uuid})'
