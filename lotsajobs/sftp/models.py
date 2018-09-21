b32884648007eb20c3e672b022ea69fe2e4be365
Patrick Heneghan <discopatrick@gmail.com>
2018-09-21 10:28:46 +0100

    Add `Request` model

--- lotsajobs/sftp/models.py
+++ lotsajobs/sftp/models.py
@@ -1,3 +1,9 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+    def __str__(self):
+        return f'Request object ({self.uuid})'
